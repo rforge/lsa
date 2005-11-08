f7c93acf366d02641bacf1ed6b3d624ae3ed0b68
wild <wild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-11-08 20:48:17 +0000

    removed pseudo_dtm, pseudo_docs will be renamed to query.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/lsa/R/pseudodtm.R
+++ trunk/R/lsa/R/pseudodtm.R
@@ -3,52 +3,13 @@
 ### -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  
 ### dependencies: library("RStem")
 ### 
+### 2005-11-08: pseudo_dtm.R renamed to query.R
+### 2005-11-08: removed pseudo_svd (now in lsa.R, renamed
+###             to foldinLSAspace()
+### 2005-11-08: removed pseudo_docs function (can be done
+###             with textmatrix, now!)
 ### 2005-08-25: added "\\[|\\]|\\{|\\}" to gsub
 ### 
-
-pseudo_docs <- function( docdir, termlist, stemming=FALSE, language="german" ) {
-    
-    # docdir: directory pointing to the files to be parsed
-    # termlist: list of allowed terms
-    # dtm: original doc-term-matrix (no weighting applied!)
-    
-    doclist = dir(docdir)
-    
-    dtm = vector( mode="numeric", length(termlist)*length(doclist) );
-    wc = length(termlist)
-    
-    for (n in doclist) {
-        
-        zz = file(paste(docdir, n, sep=""),"rt")
-        q = strsplit( gsub("[[:space:]]+", " ", gsub( "\\.|:|\\(|\\)|\\[|\\]|\\{|\\}|,|;|\\?|-|\\!|\"|\'|\`|\\^|\/", " ", tolower(readChar(zz, 1000000)) )), " ")[[1]]
-        close(zz)
-        
-        vec = vector( mode="numeric", length(termlist) )
-        
-        for ( word in q ) {
-            if (stemming) word = wordStem(word, language=language)
-            if (word != "") {
-                vec[ match(word,termlist) ] = vec[ match(word,termlist) ] + 1
-            }
-        }
-        
-        # vec now contains the original termlist
-        dtm[(length(dtm)+1):(length(dtm)+wc)] = vec
-        
-    } # // for
-    
-    # now, make it a matrix
-    dim(dtm) = c(wc,length(dtm)/wc)
-    
-    colnames(dtm) = doclist
-    rownames(dtm) = termlist
-    
-    environment(dtm) = new.env()
-    class(dtm) = "dtmatrix"
-    
-    return ( dtm )
-    
-}
 
 query <- function( qtext, termlist, stemming=FALSE, language="german" ) {
     
@@ -72,27 +33,9 @@ query <- function( qtext, termlist, stemming=FALSE, language="german" ) {
     rownames(dtm) = termlist
     
     environment(dtm) = new.env()
-    class(dtm) = "dtmatrix"
+    class(dtm) = "textmatrix"
     
     return ( dtm )
-    
-}
-
-pseudo_svd <- function( docvecs, tk, sk ) {
-    
-    ### dqs = t(docvecs) %*% tk %*% solve(diag(sk))
-    dqs = crossprod( t( crossprod(docvecs,tk) ), solve(diag(sk)) )
-    ### alternative: dqs = crossprod( docvecs, crossprod(t(tk), solve(diag(sk))) )
-    
-    ### dtm = tk %*% diag(sk) %*% t(dqs)
-    dtm = crossprod( t( crossprod(t(tk),diag(sk)) ), t(dqs) )
-    
-    dimnames(dtm) = dimnames(docvecs)
-    
-    environment(dtm) = new.env()
-    class(dtm) = "dtmatrix"
-    
-    return (dtm)
     
 }
 
