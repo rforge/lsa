5c67e84f3a2a30550f386d6f26b1caf8c81f5cca
wild <wild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-11-08 20:51:04 +0000

    * tried to add with(environment(M), { ... }) to getTriple, setTriple and delTriple (does not work)
    * added error handling to setTriple (produces warning
    if no environment exists (by checking the class,
    throws an error if input parameters are wrong)
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/lsa/R/triples.R
+++ trunk/R/lsa/R/triples.R
@@ -1,6 +1,13 @@
 ### -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  
-### triples.r v0.1
+### triples.r v0.2
 ### -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  
+### 
+### 2005-11-08:
+###   * tried to add with(environment(M), { ... })
+###     to getTriple, setTriple and delTriple (does not work)
+###   * added error handling to setTriple (produces warning
+###     if no environment exists (by checking the class,
+###     throws an error if input parameters are wrong)
 ### 
 ### 2005-08-26:
 ###   * added garbage collection to delTriple
@@ -30,29 +37,30 @@ getTriple <- function(M, subject, predicate) {
     if ( exists("triples$S",envir=environment(M)) ) {
         
         if ( ! missing(subject) ) {
-        
-            spos = which( get("triples$S",envir=environment(M)) == getSubjectId(M,subject));
+            
+            spos = which( get("triples$S", envir=environment(M)) == getSubjectId(M,subject) );
+            
             if ( ! missing(predicate)) {
-                ppos = which( get("triples$P",envir=environment(M))[spos] == tolower(predicate) );
-                objects = as.vector(get("triples$O",envir=environment(M))[spos][ppos]);
+                ppos = which( get("triples$P", envir=environment(M))[spos] == tolower(predicate) );
+                objects = as.vector(get("triples$O", envir=environment(M))[spos][ppos]);
             } else {
-                objects = list( as.vector(get("triples$P",envir=environment(M))[spos]), as.vector(get("triples$O",envir=environment(M))[spos]) );
+                objects = list( as.vector(get("triples$P", envir=environment(M))[spos]), as.vector(get("triples$O", envir=environment(M))[spos]) );
             }
             
         } else {
             if ( length(get("triples$S",envir=environment(M))) == 0) {
                 return( NULL );
             } else {
-                return ( list( as.vector(get("triples$S",envir=environment(M))), as.vector(get("triples$P",envir=environment(M))), as.vector(get("triples$O",envir=environment(M))) ) );
+                return ( list( as.vector(get("triples$S", envir=environment(M))), as.vector(get("triples$P", envir=environment(M))), as.vector(get("triples$O", envir=environment(M))) ) );
             }
         }
         
         if ( length(objects)==0 ) {
             return( NULL );
         } else return( objects );
-
-    } else return( NULL );
-
+            
+    } else return( NULL ); # if no triples exist -> return NULL
+    
 }
 
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  
@@ -66,58 +74,49 @@ setTriple <- function(M, subject, predicate, object) {
     # your own object (not created by textmatrix, 
     # please do so by:
     #    environment(M) = new.env();
+    #    class(M) = "textmatrix";
     
-    # if ( ! is.environment(environment(M)) ) return -1;
+    if ( ! class(M) == "textmatrix" ) {
+        warning("[setTriple] - You are using a matrix which has not been generated with the \nlsa package. Therefore, you have to manually add an environment to the \nmatrix you use and set the class to 'textmatrix' to be able to save triples.");
+    }
     
     # if input is vectors, check if they 
     # have the same number of elements (else break)
-    if (length(subject) != length(predicate) || length(predicate)!=length(object) || length(subject)!=length(object) ) return -1; 
+    if (length(subject) != length(predicate) || length(predicate)!=length(object) || length(subject)!=length(object) ) {
+        stop("[setTriple] - Input vectors are not of the same length!");
+    }
     
     if ( ! exists("triples$S",envir=environment(M)) ) {
         
         # if not yet existing, add 'triples$S/P/O' to 
         # environment of M and insert first triple
-        
-        assign("triples$S", factor(getSubjectId(M,subject)), envir=environment(M) );
-        assign("triples$P", factor(tolower(predicate)), envir=environment(M) );
-        assign("triples$O", factor(object), envir=environment(M) );
-        
-        #assign("triples$S", vector(mode="numeric", length(subject)), envir=environment(M) );
-        #assign("triples$S", getSubjectId(M, subject), envir=environment(M) );
-        #assign("triples$P", vector(mode="character", length(predicate)), envir=environment(M) );
-        #assign("triples$P", tolower(predicate), envir=environment(M) );
-        #assign("triples$O", vector(mode="character", length(object)), envir=environment(M) );
-        #assign("triples$O", object, envir=environment(M) );
-        
+        assign("triples$S", factor(getSubjectId(M,subject)), envir=environment(M));
+        assign("triples$P", factor(tolower(predicate)), envir=environment(M));
+        assign("triples$O", factor(object), envir=environment(M));
         
     } else {
         
         if ( !any( is.na( match(getTriple(M, subject, predicate), object)) == FALSE)  ) {
             # triple does not exist, so append
-            
-            striples = get("triples$S",envir=environment(M));
+            striples = get("triples$S", envir=environment(M));
             levels(striples) = unique(c(levels(striples), getSubjectId(M,subject)));
             striples[(length(striples)+1):(length(striples)+length(subject))] = getSubjectId(M,subject);
             assign("triples$S", striples, envir=environment(M));
             
-            ptriples = get("triples$P",envir=environment(M));
+            ptriples = get("triples$P", envir=environment(M));
             levels(ptriples) = unique(c(levels(ptriples), tolower(predicate)));
             ptriples[(length(ptriples)+1):(length(ptriples)+length(predicate))] = tolower(predicate);
             assign("triples$P", ptriples, envir=environment(M));
             
-            otriples = get("triples$O",envir=environment(M));
+            otriples = get("triples$O", envir=environment(M));
             levels(otriples) = unique(c(levels(otriples), object));
             otriples[(length(otriples)+1):(length(otriples)+length(object))] = object;
             assign("triples$O", otriples, envir=environment(M));
-            
-            #assign("triples$S", c(get("triples$S",envir=environment(M)),getSubjectId(M,subject)), envir=environment(M) );
-            #assign("triples$P", c(get("triples$P",envir=environment(M)),tolower(predicate)), envir=environment(M) );
-            #assign("triples$O", c(get("triples$O",envir=environment(M)),object), envir=environment(M) );
         }
         
     } # insert triple(s)
     
-} # // setTriple
+} # # setTriple
 
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  
 # retractTriple: remove specific triple(s) from
@@ -126,19 +125,20 @@ setTriple <- function(M, subject, predicate, object) {
 delTriple <- function(M, subject, predicate, object) {
     
     # find position
-    spos = which( get("triples$S",envir=environment(M)) == getSubjectId(M,subject));
-    ppos = which( get("triples$P",envir=environment(M))[spos] == tolower(predicate) );
-    opos = which( get("triples$O",envir=environment(M))[spos][ppos] == object);
-    origppos = ppos[opos];
-    origspos = spos[origppos];
+    spos = which( get("triples$S",envir=environment(M)) == getSubjectId(M,subject) )
+    ppos = which( get("triples$P",envir=environment(M))[spos] == tolower(predicate) )
+    opos = which( get("triples$O",envir=environment(M))[spos][ppos] == object )
+    origppos = ppos[opos]
+    origspos = spos[origppos]
     
     # retract
-    assign("triples$S", get("triples$S",envir=environment(M))[-origspos], envir=environment(M));
-    assign("triples$P", get("triples$P",envir=environment(M))[-origspos], envir=environment(M));
-    assign("triples$O", get("triples$O",envir=environment(M))[-origspos], envir=environment(M));
+    assign("triples$S", get("triples$S",envir=environment(M))[-origspos], envir=environment(M))
+    assign("triples$P", get("triples$P",envir=environment(M))[-origspos], envir=environment(M))
+    assign("triples$O", get("triples$O", envir=environment(M))[-origspos], envir=environment(M))
     
     # garbage collection
     gc()
-    
-}
+        
+} # delTriple
+
 
