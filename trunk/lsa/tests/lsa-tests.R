aefcfcdce7d857e48488fed8dea99dac1e4959c5
wild <wild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-11 13:22:41 +0000

    had to replace 'ä' with the \uxxxx sequence. nobody knows why.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@775 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/lsa/tests/lsa-tests.R
+++ trunk/lsa/tests/lsa-tests.R
@@ -200,7 +200,7 @@ unlink(td1, recursive=TRUE)
 td1 = tempfile()
 write( c("<html>\n<head></head><body background=\"#FFFFFF\">&auml;bc<h1>test></h1></body></html>"), file=td1)
 lsatest( length(rownames(textmatrix(td1, removeXML=TRUE, language="german"))) == 2, "[textmatrix] - XML removal")
-lsatest( rownames(textmatrix(td1, removeXML=TRUE, language="german"))[1] == "äbc", "[textmatrix] - html German umlaut replacement")
+lsatest( rownames(textmatrix(td1, removeXML=TRUE, language="german"))[1] == "\u00e4bc", "[textmatrix] - html German umlaut replacement")
 unlink(td1)
 
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  
