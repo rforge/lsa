6c480e06a8802f91c40e6a3c5f63b8a585113dd2
wild <wild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-28 07:34:29 +0000

    french stopwords
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@809 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/lsa/work/generate_stopwords_lists.R
+++ trunk/lsa/work/generate_stopwords_lists.R
@@ -7,6 +7,14 @@ stopwords_en = tolower( scan("stopwords_english.txt", what="character", sep = " 
 ### Thanks to Marco Kalz, Open University Netherlands!
 stopwords_nl = tolower( scan("stopwords_dutch.txt", what="character", sep = " ", quiet = TRUE, encoding = "UTF-8") )
 
+### thanks to Haykel Demnati (Haykel.Demnati@isg.rnu.tn)
+### who combined and extended this from several other lists
+
+stopwords_fr = tolower( scan("stopwords_french.txt", what="character", sep = " ", quiet = TRUE, encoding = "UTF-8") )
+stopwords_fr = unlist(strsplit(stopwords_fr,"\n"))
+stopwords_fr[1] = "$"
+
 save(stopwords_de, file="../data/stopwords_de.rda", ascii=T)
 save(stopwords_en, file="../data/stopwords_en.rda", ascii=T)
 save(stopwords_nl, file="../data/stopwords_nl.rda", ascii=T)
+save(stopwords_fr, file="../data/stopwords_fr.rda", ascii=T)
