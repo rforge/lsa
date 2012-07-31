d56ddb60034e4d2fbc6a9545b50f27bbf7189b89
fwild <fwild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-31 09:18:54 +0000

    polish stopwords added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@856 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/lsa/work/generate_stopwords_lists.R
+++ trunk/lsa/work/generate_stopwords_lists.R
@@ -1,5 +1,5 @@
 
-setwd("c:/werkstatt/lsa-package/lsa/work/")
+setwd("~/Documents/werkstatt/lsa-package/lsa/work/")
 
 stopwords_de = tolower( scan("stopwords_german.txt", what="character", sep = " ", quiet = TRUE, encoding = "UTF-8") )
 stopwords_en = tolower( scan("stopwords_english.txt", what="character", sep = " ", quiet = TRUE, encoding = "UTF-8") )
@@ -14,7 +14,17 @@ stopwords_fr = tolower( scan("stopwords_french.txt", what="character", sep = " "
 stopwords_fr = unlist(strsplit(stopwords_fr,"\n"))
 stopwords_fr[1] = "$"
 
+
+### thanks to Grażyna Paliwoda-Pękosz (paliwodg@uek.krakow.pl)
+### who extracted this stopword list from
+### http://pl.wikipedia.org/wiki/Wikipedia:Stopwords
+
+stopwords_pl = tolower( scan("stopwords_polish.txt", what="character", sep = " ", quiet = TRUE, encoding = "UTF-8") )
+stopwords_pl = unlist(strsplit(stopwords_pl,"\n"))
+stopwords_pl[1] = "$"
+
 save(stopwords_de, file="../data/stopwords_de.rda", ascii=T)
 save(stopwords_en, file="../data/stopwords_en.rda", ascii=T)
 save(stopwords_nl, file="../data/stopwords_nl.rda", ascii=T)
 save(stopwords_fr, file="../data/stopwords_fr.rda", ascii=T)
+save(stopwords_pl, file="../data/stopwords_pl.rda", ascii=T)
