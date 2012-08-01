45185b93c1bf2d4a16525c4a821f96dea3555150
fwild <fwild@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-01 10:21:21 +0000

    windows fix charset polish test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lsa@861 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lsa/tests/lsa-tests.R
+++ pkg/lsa/tests/lsa-tests.R
@@ -274,5 +274,5 @@ load("polski.RData")
 write(polski, file=td1)
 polski = textvector(td1, language="polish")
 polski['terms']
-lsatest( tolower(polski['terms']) == "testąąęęóóććłłńńśśźźżż", "[textvector] - polish language support" )
+lsatest( polski['terms'] == "testąąęęóóććłłńńśśźźżż", "[textvector] - polish language support" )
 unlink(td1)
